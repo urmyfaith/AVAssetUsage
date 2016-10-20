2b342150dda7e8c26ccc0a1446952eec39f0bf80
zx <zuoxue@qq.com>
2016-10-20 09:17:36 +0800

    Initial Commit

---
+++ AVAssetUsage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAssetUsage
+//
+//  Created by zx on 10/20/16.
+//  Copyright © 2016 z2xy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
