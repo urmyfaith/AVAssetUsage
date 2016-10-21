b559cc2e8bdf6079037b31b7007c99d9e7782c6b
zx <zuoxue@qq.com>
2016-10-21 15:36:47 +0800

    add Changing_the_Compositions_Backgournd_Color

--- AVAssetUsage/EditingViewController.m
+++ AVAssetUsage/EditingViewController.m
@@ -34,7 +34,9 @@
 
 //    [self Generating_a_Volume_Ramp];//只有音频轨道
 
-    [self Generating_a_Volume_Ramp_Verison_2];//音频+视频轨道
+//    [self Generating_a_Volume_Ramp_Verison_2];//音频+视频轨道
+
+    [self Changing_the_Compositions_Backgournd_Color];
 }
 
 -(AVURLAsset *)Creating_an_Asset_Object
@@ -202,6 +204,26 @@
 
 }
 
+-(NSURL *)generate_save_url
+{
+    static NSDateFormatter *kDateFormatter;
+    if (!kDateFormatter) {
+        kDateFormatter = [[NSDateFormatter alloc] init];
+        [kDateFormatter setDateFormat:@"yyyy-MM-dd-HH-mm-ss"];
+    }
+
+    NSURL *saveURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
+
+    saveURL = [saveURL URLByAppendingPathComponent:[kDateFormatter stringFromDate:[NSDate date]]];
+
+    NSString *fileExtensitionString  = CFBridgingRelease(UTTypeCopyPreferredTagWithClass((CFStringRef)AVFileTypeQuickTimeMovie, kUTTagClassFilenameExtension));
+
+    saveURL = [saveURL URLByAppendingPathExtension:fileExtensitionString];
+
+    ZXLog(@"save url:%@",saveURL);
+    return saveURL;
+}
+
 -(void)Generating_a_Volume_Ramp_Verison_2
 {
 
@@ -237,24 +259,9 @@
 
     mutableAudioMix.inputParameters = @[mixParameters];
 
-    static NSDateFormatter *kDateFormatter;
-    if (!kDateFormatter) {
-        kDateFormatter = [[NSDateFormatter alloc] init];
-        [kDateFormatter setDateFormat:@"yyyy-MM-dd-HH-mm-ss"];
-    }
-
     AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:mutableComposition presetName:AVAssetExportPresetHighestQuality];
 
-    NSURL *saveURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
-
-    saveURL = [saveURL URLByAppendingPathComponent:[kDateFormatter stringFromDate:[NSDate date]]];
-
-    NSString *fileExtensitionString  = CFBridgingRelease(UTTypeCopyPreferredTagWithClass((CFStringRef)AVFileTypeQuickTimeMovie, kUTTagClassFilenameExtension));
-
-    saveURL = [saveURL URLByAppendingPathExtension:fileExtensitionString];
-
-    ZXLog(@"save url:%@",saveURL);
-
+    NSURL *saveURL = [self generate_save_url];
     exporter.outputURL = saveURL;
 
     exporter.outputFileType = AVFileTypeQuickTimeMovie;
@@ -302,6 +309,93 @@
 - (void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
 {
     ZXLog(@"compeleteSaveVideo: %@",videoPath);
+}
+
+-(void)Performing_Custom_Video_Processing
+{
+    //通过音频混合,只需要一个AVMutableVideoCompositio来完成自定义的视频处理.
+    //通过 video composition 可以完成:
+    // 1) 渲染大小, 2)sacale 3)帧率调整.
+}
+
+-(void)Changing_the_Compositions_Backgournd_Color
+{
+    AVAsset *videoAsset = [self Creating_an_Asset_Object];
+
+    AVAssetTrack *audioAssetTrack = [[videoAsset tracksWithMediaType:AVMediaTypeAudio] firstObject];
+    AVAssetTrack *videoAssetTrack = [[videoAsset tracksWithMediaType:AVMediaTypeVideo] firstObject];
+
+    AVMutableComposition *mutableComposition = [AVMutableComposition composition];
+
+    AVMutableCompositionTrack *mutableCompositionAudioTrack = [mutableComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
+    AVMutableCompositionTrack *mutableCompositionVideoTrack = [mutableComposition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
+
+
+    [mutableCompositionAudioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, audioAssetTrack.timeRange.duration)
+                                          ofTrack:audioAssetTrack
+                                           atTime:kCMTimeZero
+                                            error:nil];
+
+    [mutableCompositionVideoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoAssetTrack.timeRange.duration)
+                                          ofTrack:videoAssetTrack
+                                           atTime:kCMTimeZero
+                                            error:nil];
+
+    //keypoint  code here.
+    AVMutableVideoCompositionInstruction *backgroundColorCompositionInstruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
+    backgroundColorCompositionInstruction.timeRange = CMTimeRangeMake(kCMTimeZero,mutableComposition.duration);
+    backgroundColorCompositionInstruction.backgroundColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:0.5].CGColor;
+    //keypoint code ene.
+
+    AVMutableVideoComposition *mutableVideoComposition = [AVMutableVideoComposition videoComposition];
+    mutableVideoComposition.instructions = @[backgroundColorCompositionInstruction];
+    mutableVideoComposition.renderSize = videoAssetTrack.naturalSize;
+    mutableVideoComposition.frameDuration = videoAssetTrack.timeRange.duration;
+
+    AVAssetExportSession *exporter = [[AVAssetExportSession alloc]initWithAsset:mutableComposition presetName:AVAssetExportPresetHighestQuality];
+
+    NSURL *saveURL = [self generate_save_url];
+
+    exporter.outputURL = saveURL;
+    exporter.outputFileType = AVFileTypeQuickTimeMovie;
+    exporter.shouldOptimizeForNetworkUse = YES;
+    exporter.videoComposition = mutableVideoComposition;
+
+    [exporter exportAsynchronouslyWithCompletionHandler:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            AVAssetExportSessionStatus status = exporter.status;
+            ZXLog(@"exporter status = %zd",status);
+            switch (status) {
+                case AVAssetExportSessionStatusUnknown: {
+                    ZXLog(@"AVAssetExportSessionStatusUnknown")
+                    break;
+                }
+                case AVAssetExportSessionStatusWaiting: {
+                    ZXLog(@"AVAssetExportSessionStatusWaiting")
+                    break;
+                }
+                case AVAssetExportSessionStatusExporting: {
+                    ZXLog(@"AVAssetExportSessionStatusExporting")
+                    break;
+                }
+                case AVAssetExportSessionStatusCompleted: {
+                    ZXLog(@"AVAssetExportSessionStatusCompleted")
+                    if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(saveURL.path)) {
+                        UISaveVideoAtPathToSavedPhotosAlbum(saveURL.path, self, @selector(video:didFinishSavingWithError:contextInfo:), nil);
+                    }
+                    break;
+                }
+                case AVAssetExportSessionStatusFailed: {
+                    ZXLog(@"Export failed: %@", [[exporter error] localizedDescription]);
+                    break;
+                }
+                case AVAssetExportSessionStatusCancelled: {
+                    ZXLog(@"AVAssetExportSessionStatusCancelled")
+                    break;
+                }
+            }
+        });
+    }];
 }
 
 @end
